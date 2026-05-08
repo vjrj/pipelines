f59286dfbc02ac2a92eeeb5b259558de8ca7f3c6
Adam Collins <adam.collins@csiro.au>
2026-05-08 11:32:46 +1000

    Simpliy GeocodeShpConfig and ShapeFile constructors

--- livingatlas/pipelines/src/main/java/au/org/ala/kvs/GeocodeShpConfig.java
+++ livingatlas/pipelines/src/main/java/au/org/ala/kvs/GeocodeShpConfig.java
@@ -1,28 +1,16 @@
 package au.org.ala.kvs;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import java.io.Serializable;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /** Store configuration of SHP files including fields for ALA Country/State interpretation */
 @Data
+@NoArgsConstructor
 public class GeocodeShpConfig implements Serializable {
 
   private ShapeFile country;
   private ShapeFile eez;
   private ShapeFile stateProvince;
   private ShapeFile biome;
-
-  @JsonCreator
-  public GeocodeShpConfig(
-      @JsonProperty("country") ShapeFile country,
-      @JsonProperty("eez") ShapeFile eez,
-      @JsonProperty("stateProvince") ShapeFile stateProvince,
-      @JsonProperty("biome") ShapeFile biome) {
-    this.country = country;
-    this.eez = eez;
-    this.stateProvince = stateProvince;
-    this.biome = biome;
-  }
 }
